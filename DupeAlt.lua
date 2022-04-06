ce9ab10d09bf853bba4d039008c59866cfee2886
MP4000 <103048038+MP4000@users.noreply.github.com>
2022-04-06 18:52:25 -0500

    Update DupeAlt.lua

--- DupeAlt.lua
+++ DupeAlt.lua
@@ -1,2 +1,2 @@
-_G.Loader_Key = "Gu54826550433kh"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ModProgrammMiked/protectedscripts/main/get-script-with-key.lua"))()
+_G.AuthKey = "6791905D-D3C4-47E9-899D-A73A1117C725"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobIoxScripts/GUI/main/load.lua"))()
